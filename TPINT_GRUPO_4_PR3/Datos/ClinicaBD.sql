b8874a93ea046ec9cde27101ebf8c65c7d1876eb
Luca Gauna <83098830+lucagauna@users.noreply.github.com>
2025-06-19 20:36:39 -0300

    upd bd
    aca les dejo la base de datos asi van testeando cositas

--- TPINT_GRUPO_4_PR3/Datos/ClinicaBD.sql
+++ TPINT_GRUPO_4_PR3/Datos/ClinicaBD.sql
@@ -1,124 +1,140 @@
-CREATE DATABASE ClinicaDB
+CREATE DATABASE ClinicaDB;
 GO
-USE ClinicaDB
--- Tabla Roles
+
+USE ClinicaDB;
+GO
+
+-- Tabla: Roles
 CREATE TABLE Roles (
-  idRol INT PRIMARY KEY,
-  descripcion VARCHAR(25)
+    idRol INT PRIMARY KEY,
+    descripcion VARCHAR(25)
 );
 
--- Tabla Sexos
+-- Tabla: Sexos
 CREATE TABLE Sexos (
-  idSexo INT PRIMARY KEY,
-  descripcion VARCHAR(25)
+    idSexo INT PRIMARY KEY,
+    descripcion VARCHAR(25)
 );
 
--- Tabla Paises
+-- Tabla: Paises
 CREATE TABLE Paises (
-  idPais INT PRIMARY KEY,
-  nombrePais VARCHAR(25),
-  gentilicio VARCHAR(25)
+    idPais INT PRIMARY KEY,
+    nombrePais VARCHAR(25),
+    gentilicio VARCHAR(25) UNIQUE
 );
 
--- Tabla Provincias
+-- Tabla: Provincias
 CREATE TABLE Provincias (
-  idProvincia INT PRIMARY KEY,
-  nombreProvincia NVARCHAR(255) UNIQUE
+    idProvincia INT PRIMARY KEY,
+    nombreProvincia VARCHAR(25) UNIQUE
 );
 
--- Tabla Localidades
+-- Tabla: Localidades
 CREATE TABLE Localidades (
-  idLocalidad INT PRIMARY KEY,
-  nombreLocalidad NVARCHAR(255),
-  idProvincia INT,
-  FOREIGN KEY (idProvincia) REFERENCES Provincias(idProvincia)
+    idLocalidad INT PRIMARY KEY,
+    nombreLocalidad VARCHAR(25),
+    idProvincia INT FOREIGN KEY REFERENCES Provincias(idProvincia)
 );
 
--- Tabla Persona
+-- Tabla: Persona
 CREATE TABLE Persona (
-  DNI NVARCHAR(255) PRIMARY KEY,
-  nombre VARCHAR(25),
-  apellido VARCHAR(25),
-  sexo INT,
-  direccion VARCHAR(25),
-  idLocalidad INT,
-  fechaNacimiento DATE,
-  idPais INT,
-  FOREIGN KEY (sexo) REFERENCES Sexos(idSexo),
-  FOREIGN KEY (idLocalidad) REFERENCES Localidades(idLocalidad),
-  FOREIGN KEY (idPais) REFERENCES Paises(idPais)
+    DNI VARCHAR(20) PRIMARY KEY,
+    nombre VARCHAR(25),
+    apellido VARCHAR(25),
+    sexo INT FOREIGN KEY REFERENCES Sexos(idSexo),
+    direccion VARCHAR(25),
+    idLocalidad INT FOREIGN KEY REFERENCES Localidades(idLocalidad),
+    fechaNacimiento DATE,
+    nacionalidad VARCHAR(25) FOREIGN KEY REFERENCES Paises(gentilicio)
 );
 
--- Tabla Usuario (Admins y Médicos)
+-- Tabla: Usuario
 CREATE TABLE Usuario (
-  DNI NVARCHAR(255) PRIMARY KEY,
-  idRol INT,
-  nombreUsuario VARCHAR(25),
-  contrasenia VARCHAR(25),
-  ultimoIngreso DATETIME,
-  alta DATETIME,
-  FOREIGN KEY (DNI) REFERENCES Persona(DNI),
-  FOREIGN KEY (idRol) REFERENCES Roles(idRol)
+    DNI VARCHAR(20) PRIMARY KEY,
+    idRol INT,
+    nombreUsuario VARCHAR(25),
+    contrasenia VARCHAR(25),
+    ultimoIngreso DATETIME,
+    alta DATETIME,
+    FOREIGN KEY (idRol) REFERENCES Roles(idRol),
+    FOREIGN KEY (DNI) REFERENCES Persona(DNI)
 );
 
-
--- Tabla Especialidades
-CREATE TABLE Especialidades (
-  idEspecialidad INT PRIMARY KEY,
-  descripcion VARCHAR(25)
-);
-
--- Tabla Medico
-CREATE TABLE Medico (
-  DNI NVARCHAR(255) PRIMARY KEY,
-  Legajo NVARCHAR(255) UNIQUE,
-  idEspecialidad INT,
-  FOREIGN KEY (DNI) REFERENCES Usuario(DNI),
-  FOREIGN KEY (idEspecialidad) REFERENCES Especialidades(idEspecialidad)
-);
-
--- Tabla Telefonos
+-- Tabla: Telefonos
 CREATE TABLE Telefonos (
-  DNI NVARCHAR(255),
-  telefono VARCHAR(25) UNIQUE,
-  PRIMARY KEY (DNI, telefono),
-  FOREIGN KEY (DNI) REFERENCES Persona(DNI)
+    idPersona VARCHAR(20),
+    telefono VARCHAR(25),
+    PRIMARY KEY (idPersona, telefono),
+    FOREIGN KEY (idPersona) REFERENCES Persona(DNI)
 );
 
--- Tabla Correos
+-- Tabla: Correos
 CREATE TABLE Correos (
-  DNI NVARCHAR(255),
-  correo VARCHAR(25) UNIQUE,
-  PRIMARY KEY (DNI, correo),
-  FOREIGN KEY (DNI) REFERENCES Persona(DNI)
+    idPersona VARCHAR(20),
+    correo VARCHAR(25),
+    PRIMARY KEY (idPersona, correo),
+    FOREIGN KEY (idPersona) REFERENCES Persona(DNI)
 );
 
--- Tabla Jornadas
+-- Tabla: Especialidades
+CREATE TABLE Especialidades (
+    idEspecialidad INT PRIMARY KEY,
+    descripcion VARCHAR(25)
+);
+
+-- Tabla: Medico
+CREATE TABLE Medico (
+    DNI VARCHAR(20),
+    Legajo VARCHAR(20),
+    idEspecialidad INT,
+    PRIMARY KEY (DNI, Legajo),
+    UNIQUE (DNI),
+    UNIQUE (Legajo),
+    FOREIGN KEY (DNI) REFERENCES Persona(DNI),
+    FOREIGN KEY (idEspecialidad) REFERENCES Especialidades(idEspecialidad)
+);
+
+-- Tabla: Jornadas
 CREATE TABLE Jornadas (
-  Legajo NVARCHAR(255),
-  DiaSemana NVARCHAR(255),
-  rangoHorario TIME,
-  PRIMARY KEY (Legajo, DiaSemana, rangoHorario),
-  FOREIGN KEY (Legajo) REFERENCES Medico(Legajo)
+    Legajo VARCHAR(20),
+    DiaSemana VARCHAR(20),
+    rangoHorario TIME,
+    PRIMARY KEY (Legajo, DiaSemana, rangoHorario),
+    FOREIGN KEY (Legajo) REFERENCES Medico(Legajo)
 );
 
--- Tabla EstadoTurnos
+-- Tabla: EstadoTurnos
 CREATE TABLE EstadoTurnos (
-  idEstado INT PRIMARY KEY,
-  descripcion VARCHAR(25)
+    idEstado INT PRIMARY KEY,
+    valorEstado BIT,
+    descripcion VARCHAR(25)
 );
 
--- Tabla Turnos
- CREATE TABLE Turnos (
-  idTurno INT IDENTITY PRIMARY KEY,
-  Legajo NVARCHAR(255),
-  DNIPaciente NVARCHAR(255),
-  fechaPactada DATE,
-  hora TIME,
-  estado INT,
-  observacion VARCHAR(200),
-  FOREIGN KEY (Legajo) REFERENCES Medico(Legajo),
-  FOREIGN KEY (DNIPaciente) REFERENCES Persona(DNI),
-  FOREIGN KEY (estado) REFERENCES EstadoTurnos(idEstado),
-  CONSTRAINT UQ_Turno_Unico UNIQUE (Legajo, fechaPactada, hora) --> Para que no se pisen los turnos.
+-- Tabla: ObraSocial
+CREATE TABLE ObraSocial (
+    idObraSocial INT PRIMARY KEY,
+    nombre VARCHAR(25)
+);
+
+-- Tabla: Paciente
+CREATE TABLE Paciente (
+    DNI VARCHAR(20) PRIMARY KEY,
+    ObraSocial INT,
+    ultimaAtencion DATETIME,
+    alta DATETIME,
+    FOREIGN KEY (DNI) REFERENCES Persona(DNI),
+    FOREIGN KEY (ObraSocial) REFERENCES ObraSocial(idObraSocial)
+);
+
+-- Tabla: Turnos
+CREATE TABLE Turnos (
+    Legajo VARCHAR(20),
+    DNIPaciente VARCHAR(20),
+    fechaPactada DATE,
+    estado INT,
+    observacion VARCHAR(200),
+    diagnostico VARCHAR(50),
+    FOREIGN KEY (Legajo) REFERENCES Medico(Legajo),
+    FOREIGN KEY (DNIPaciente) REFERENCES Paciente(DNI),
+    FOREIGN KEY (estado) REFERENCES EstadoTurnos(idEstado)
 );
